fcdc252a746bc435837aedfda648a041147b982f
Ilya Kuznetsov <99133715+Ilya-Kuznetsov2@users.noreply.github.com>
2024-12-19 22:39:20 +0300

    Update task_1.cu

--- task_1.cu
+++ task_1.cu
@@ -1,3 +1,5 @@
+#include <cuda_runtime.h>
+#include <cuda.h>
 #include <cmath>
 #include <iostream>
 #include <fstream>
@@ -17,26 +19,31 @@ __host__ void WriteState(FILE *out, int n, float time, float *positions) {
 }
 
 __global__ void CalcForces(int n, float *m, float *positions, float *totalF) {
-    
-    for (int first_point = 0; first_point < n; ++first_point) 
-        for (int second_point = 0; second_point < i; ++second_point) {
-            float dist_x = positions[first_point * 2] - positions[second_point * 2];
-            float dist_y = positions[first_point * 2 + 1] - positions[second_point * 2 + 1];
-            float norm = powf(sqrtf(dist_x * dist_x + dist_y * dist_y), 3.0f) + 1e-12;
-            float f_coef = G * m[first_point] * m[second_point] / norm;
-            totalF[2 * first_point] += dist_x * f_coef;
-            totalF[2 * first_point + 1] += dist_y * f_coef;
-            totalF[2 * second_point] -= dist_x * f_coef;
-            totalF[2 * second_point + 1] -= dist_y * f_coef;
-        }
+    int first_point = blockDim.x * blockIdx.x + threadIdx.x;
+    int second_point = blockDim.y * blockIdx.y + threadIdx.y;
+    if (first_point >= second_point || first_point >= n) {
+        return;
+    }
+    float dist_x = positions[first_point * 2] - positions[second_point * 2];
+    float dist_y = positions[first_point * 2 + 1] - positions[second_point * 2 + 1];
+    float norm = powf(sqrtf(dist_x * dist_x + dist_y * dist_y), 3.0f) + 1e-12;
+    float f_coef = G * m[first_point] * m[second_point] / norm;
+    atomicAdd(&totalF[2 * first_point], dist_x * f_coef);
+    atomicAdd(&totalF[2 * first_point + 1], dist_y * f_coef);
+    atomicAdd(&totalF[2 * second_point], - dist_x * f_coef);
+    atomicAdd(&totalF[2 * second_point + 1], - dist_y * f_coef);
 }
 
 __global__ void CalcState(int n, float* m, float *positions, float *V, float *F) {
-    for (int i = 0; i < 2 * n; ++i) {
-        positions[i] += V[i] * dt;
-        V[i] += F[i] / m[i / 2] * dt;
-        F[i] = 0.0f;
+    int point_index = blockDim.x * blockIdx.x + threadIdx.x;
+    if (point_index >= n) {
+        return;
     }
+    int point_coord = threadIdx.y;
+    int i = 2 * point_index + point_coord;
+    positions[i] += V[i] * dt;
+    V[i] += F[i] / m[point_index] * dt;
+    F[i] = 0.0f;
 }
 
 int main() {
@@ -59,7 +66,6 @@ int main() {
     float *m = (float*) malloc(n * sizeof(float));
     float *positions = (float*) malloc(2 * n * sizeof(float));
     float *V = (float*) malloc(2 * n * sizeof(float));
-    float *F = (float*) malloc(2 * n * sizeof(float));
 
     for (int i = 0; i < n; ++i) {
         fscanf(input, "%f %f %f %f %f", &m[i], &positions[2 * i], &positions[2 * i + 1], &V[2 * i], &V[2 * i + 1]);
@@ -67,7 +73,16 @@ int main() {
 
     fclose(input);
 
-    memset(F, 0.0f, 2 * n * sizeof(float));
+    float* d_m, *d_positions, *d_V, *d_F;
+    cudaMalloc(&d_m, n * sizeof(float));
+    cudaMalloc(&d_positions, 2 * n * sizeof(float));
+    cudaMalloc(&d_V, 2 * n * sizeof(float));
+    cudaMalloc(&d_F, 2 * n * sizeof(float));
+
+    cudaMemcpy(d_m, m, n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_positions, positions, 2 * n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_V, V, 2 * n * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemset(d_F, 0.0f, 2 * n * sizeof(float));
 
     FILE *out = fopen("output.csv", "w");
     if (!out) {
@@ -92,8 +107,9 @@ int main() {
     dim3 forces_block_size = {thread_count, thread_count};
 
     for (float time = 0; time < final_time; time += dt) {
-        CalcForces(n, d_m, d_positions, d_F);
-        CalcState(n, d_m, d_positions, d_V, d_F);
+        CalcForces<<<block_count, forces_block_size>>>(n, d_m, d_positions, d_F);
+        CalcState<<<block_count, point_size>>>(n, d_m, d_positions, d_V, d_F);
+        cudaMemcpy(positions, d_positions, 2 * n * sizeof(float), cudaMemcpyDeviceToHost);
         if (write_flag) { WriteState(out, n, time + dt, positions); }
     }
 
